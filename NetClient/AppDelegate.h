9c62d521d51a10f2c6926fea9112536731da6062
김창기/앱기술개발팀 <cgkim@nhnent.com>
2015-04-06 14:04:32 +0900

    Initial Commit

---
+++ NetClient/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NetClient
+//
+//  Created by cgkim on 2015. 4. 6..
+//  Copyright (c) 2015년 cgkim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NetClient/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NetClient
+//
+//  Created by cgkim on 2015. 4. 6..
+//  Copyright (c) 2015년 cgkim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
