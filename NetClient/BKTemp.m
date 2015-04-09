1a1c4fbd1babb05eed07f3c0096b5b13aec61109
김창기/앱기술개발팀 <cgkim@nhnent.com>
2015-04-09 18:16:27 +0900

    test

---
+++ NetClient/BKTemp.m
@@ -0,0 +1,24 @@
+/*
+ *  BKTemp.m
+ *  NetClient
+ *
+ *  Created by bearkode on 2015. 4. 8..
+ *  Copyright (c) 2015 bearkode. All rights reserved.
+ *
+ */
+
+#import "BKTemp.h"
+
+
+@implementation BKTemp
+
+
+- (void)dealloc
+{
+    NSLog(@"Dealloc");
+    
+    [super dealloc];
+}
+
+
+@end
